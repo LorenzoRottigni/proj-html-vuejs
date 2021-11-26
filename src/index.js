97fd409fe5d5034e1a9a679e78393f7412c16d7d
Lorenzo Rottigni <82819521+LorenzoRottigni@users.noreply.github.com>
2021-11-26 01:01:49 +0100

    Security fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 const headers = {
   "Accept": "application/json, text/plain, */*",
-  "Authorization": "Basic ZnQ5UVloMjlOWmxoSFJYU0FGVldUM0pJdnFTUktlQWg3ejhLZ2dNZzp1U3Y0WTdKaGEzV2tONktNSWZmWnhHbTVrb0tvYTdMbThpVm5qOWVoNlhNaG9wQlJRMUNqMXZkaUI1SlNvTFU1bk0yVURXMjZ2OFVYeFM2QmRoaDA0YjhEaXV1MG5QZ3RxY3VadFhXSXVzM29ZY0Nla2ZiYkpDSmJUMUVoZmlvWQ==",
+  "Authorization": "Auth base64",
   "Content-Type": "application/json;charset=utf-8"
 }
 
